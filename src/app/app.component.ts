71ffc88bd743efe6380eaada0e179eb7041514c7
caraclarke <cara.clarke8@gmail.com>
2016-07-21 17:08:21 -0400

    bring routes together, set default page route
    make app.component.ts main routing outlet
    
    configure routes in main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  directives: [ROUTER_DIRECTIVES],
+  template: "<router-outlet></router-outlet>"
 })
 export class AppComponent {
   title = 'app works!';
 }
+
+/*
+
+code says we want this ts file to act as main routing outlet
+all child pages and routes will flow through this file
+this is because of <router-outlet> tag above
+
+still need to configure routes
+
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { provideRouter, RouterConfig } from '@angular/router';
 import { AppComponent, environment } from './app/';
+
+import { ListComponent } from './app/list/list.component';
+import { CreateComponent } from './app/create/create.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+export const AppRoutes: RouterConfig = [
+  { path: "", component: ListComponent },
+  { path: "create", component: CreateComponent }
+]
+
+bootstrap(AppComponent, [provideRouter(AppRoutes)]);
+
+/*
+
+import every component we want to link
+when routes are defined, route with blank path is default component
+inject routes when boostrapping
+
+*/
